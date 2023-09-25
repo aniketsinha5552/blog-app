60237e13199cffe8613d2451938fd6b7f399aca4
aniketsinha5552 <104712880+aniketsinha5552@users.noreply.github.com>
2023-09-25 20:59:37 +0530

    Navbar, ThemeContext

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import styles from "./authLinks.module.css"
 import Link from 'next/link'
 
 const AuthLinks = () => {
   const user=false
+
+  const [open,setOpen]= useState()
   return (
     <>
-      {!user? <Link href="/login">Login</Link>
+      {!user? <Link className={styles.link} href="/login">Login</Link>
       : <>
-      <Link href="/write">Write</Link>
+      <Link className={styles.link} href="/write">Write</Link>
       <span className={styles.link}>Logout</span>
       </>}
+      <div className={styles.burger} onClick={()=>setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open &&(
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          {!user? <Link href="/login">Login</Link>
+          : <>
+          <Link href="/write">Write</Link>
+          <span className={styles.link}>Logout</span>
+          </>}
+        </div>
+      )}
     </>
   )
 }
