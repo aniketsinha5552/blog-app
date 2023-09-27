f24369a46437c38b1e38ab5873d94eebde71d1aa
aniketsinha5552 <104712880+aniketsinha5552@users.noreply.github.com>
2023-09-27 21:12:32 +0530

    category page

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from "./blog.module.css"
+import CardList from '@/components/cardList/CardList'
+import Menu from '@/components/menu/Menu'
+
+const page = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Style Blog</h1>
+      <div className={styles.content}>
+        <CardList/>
+        <Menu/>
+      </div>
+    </div>
+  )
+}
+
+export default page
